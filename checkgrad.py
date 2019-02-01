115569883d0cf72a63f3d98480576ee266fb0b7e
scwolof <scw.olofsson@gmail.com>
2019-02-01 17:09:04 +0000

    Freedom with dimensions

--- checkgrad.py
+++ checkgrad.py
@@ -7,57 +7,58 @@ using comparison with finite difference gradients.
 (C) 2017 by Simon Olofsson
 """
 
-def gradient (f, X, eps=1e-6, flatten=False):
+def gradient (f, X, eps=1e-6):
     """
     Check gradients of function.
     checkgrad.gradient (f, X, eps=1e-6)
 
     Inputs:
         f           Function, returns Y(X), dY/dX.
-                    X       [n, D] 
-                    Y       [n, (E)]
-                    dY/dX   [n, (E,) D]
-        X           [n, D] input
+                    X       [D1, ..., Dn] 
+                    Y       [E1, ..., Em]
+                    dY/dX   [E1, ..., Em, D1, ..., Dn]
         eps         Step size
-        flatten     Return a more print-friendly result.
 
     Outputs:
         - Default:
         d           Relative difference |dy-dh| / |dy+dh|
         dy          Analytical gradients
         dh          Numerical gradients
-        - Flattened:
-        Numpy array np.c_[d.flatten(), dy.flatten(), dh.flatten()]
+        flattened   d, dy and dh flattened into three columns
     """
-    n, D = X.shape
+    shape_in = X.shape
+    if not isinstance(shape_in, list):
+        shape_in = list(shape_in)
 
     # Analytical gradient
     y, dy = f(X)
-    assert y.shape[0] == n
-    if y.ndim == 1:
-        assert dy.shape == (n,D)
-    else:
-        E = y.shape[1]
-        assert dy.shape == (n,E,D)
-
-    # Numerical gradients using finite differences
+    shape_out = y.shape
+    if not isinstance(shape_out, list):
+        shape_out = list(shape_out)
+    assert list(dy.shape) == shape_out + shape_in
     dh = np.zeros( dy.shape )
-    for k in range( D ):
+
+    # Indices to cycle through
+    inds = [ np.arange(D, dtype=int) for D in shape_in ]
+    inds = np.meshgrid( *inds )
+    inds = np.stack([ i.flatten() for i in inds ]).T
+    # Numerical gradients using finite differences
+    for ind in inds:
         # Step array
         t      = np.zeros( X.shape )
-        t[:,k] = eps
+        t[ind] = eps
         # Evaluate function
         yp = f(X + t)[0]
         ym = f(X - t)[0]
         # Finite difference computation
-        dh[...,k] = (yp - ym) / (2 * eps)
+        expr = ':,' * len(shape_out) + ','.join(['%d'%i for i in ind])
+        exec(r"dh["+expr+r"] = (yp - ym) / (2 * eps)")
 
     # Compute difference in gradients
     d = np.sqrt( (dy-dh)**2 / ((dy+dh)**2 + 1e-300) )
 
-    if flatten: 
-        return np.c_[d.flatten(), dy.flatten(), dh.flatten()]
-    return d, dy, dh
+    flattened = np.c_[d.flatten(), dy.flatten(), dh.flatten()]
+    return d, dy, dh, flattened
     
 
 
